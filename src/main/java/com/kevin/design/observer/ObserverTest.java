29fd537d4679eaa1a1bc953b3ccb740835e6502d
Kevin <Kevin_developer@163.com>
2018-09-20 13:51:31 +0800

    合入原因：添加观察者模式。 修改人：Kevin

--- src/main/java/com/kevin/design/observer/ObserverTest.java
+++ src/main/java/com/kevin/design/observer/ObserverTest.java
@@ -2,7 +2,7 @@ package com.kevin.design.observer;
 
 /**
  * @ClassName: ObserverTest
- * @Description: 测试类
+ * @Description: 观察者模式 - 测试类
  * @Author: Kevin
  * @Date: 2018/9/20 10:03
  */
@@ -13,6 +13,14 @@ public class ObserverTest {
         subject.addObserver(new Observer1Impl());
         subject.addObserver(new Observer2Impl());
         subject.operation();
+        System.out.println("======================");
+        Observer extraObserver = new Observer2Impl();
+        subject.addObserver(extraObserver);
+        subject.operation();
+        System.out.println("======================");
+        subject.delObserver(extraObserver);
+        subject.operation();
+
     }
 
 }
